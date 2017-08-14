4cf43dc8d5b1c205e55ef955cd5afa1e2bcd7359
abhinavkondapalli <30722654+abhinavkondapalli@users.noreply.github.com>
2017-08-14 15:51:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/article-one',function (req,res) {
      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function (req,res) {
-   res.send('article two is requested and will not be served');
-});
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+})
 
 app.get('/article-three',function (req,res) {
     res.send('article three is requested and will not be served');    

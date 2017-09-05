483321ff0f3780ef956e623f811d61b9ebe8a569
abhinavkondapalli <30722654+abhinavkondapalli@users.noreply.github.com>
2017-09-06 00:03:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -42,12 +42,6 @@ app.get('/article-three',function (req,res) {
       }
       })
       });
-    
-    
-    
-    
-    
-    
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', '.main.js'));
 });
